afca7ca186686282f8cee5b017cd481d81da9cbd
ayush <ayushmatmotgill@gmail.com>
2022-12-07 03:31:23 -0800

    f

--- src/Pages/OfferDetails/OfferDetails.js
+++ src/Pages/OfferDetails/OfferDetails.js
@@ -240,13 +240,35 @@ console.log(term)
     <h5>Lock In Period</h5>
     <select className='firstSelet1'>
                 <option>No.of Months</option>
-                <option value={'1'}>1</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
             </select>
 
    <h5 className='offerTitle'>Notice Period</h5>
    <select className='thirdSelect1' onChange={(e)=>setUserType(e.target.value)} required value={userType}>
                 <option>No.of Days</option>
-                <option value={'12'}>12</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
             </select>
 </div>
 
